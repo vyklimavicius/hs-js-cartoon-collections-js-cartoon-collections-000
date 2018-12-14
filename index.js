f5a9484360a1e34aad5ed5133ce3e4e0007adaee
Vytautas Klimavicius <vyklimavicius@gmail.com>
2018-12-14 23:39:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 function dwarfRollCall(dwarves) {
+  let string = '';
+  for (let i = 0; i < dwarves.length; i++){
+    string += (`${i+1}. ${dwarves[i]} `);
+  }
+  return string;
 }
 
+
 function summonCaptainPlanet(planeteerCalls){
+let array = [];
+ for (let i = 0; i < planeteerCalls.length; i++){
+    array.push(planeteerCalls[i].toUpperCase() + '!');
+}
+    return array;
 }
 
 function longPlaneteerCalls(words) {
+for (let i = 0; i < words.length; i++){
+    if (words[i].length > 4){
+      return true;
+    }
+}
+      return false;
 }
 
 function findTheCheese (foods) {
+  for (let i = 0; i < foods.length; i++){
+  if(foods[i] === 'cheddar' || foods[i] === 'gouda' || foods[i] === 'camembert' ){
+    return foods[i];
+  }
+}
+    return 'no cheese!';
 }
